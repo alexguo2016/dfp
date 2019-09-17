92c036292dbf33c07506c0f5b89f8e5a622db829
alexguo2016 <240791268@qq.com>
2019-09-17 14:17:05 +0800

    think in java begin

---
+++ think-in-java/java_code/HelloDate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class HelloDate {
+	public static void main(String[] args) {
+		int s = 23343;
+		System.out.println("Hello it's: " + s);	
+		// System.out.println(new Date());
+	}
+}
